cedc4d74288bfd6d2ed7a73bf4bf0f4179d9d2ae
benjamin.mouchet <benjamin.mouchet@he-arc.ch>
2022-06-06 13:29:37 +0200

    fixed merge conflict

--- Game/Player.cs
+++ Game/Player.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Characters;
+using Entities.Characters.Tier1;
 using Game.Server;
 using Newtonsoft.Json;
 using System;
@@ -50,6 +51,8 @@ namespace Game
 		public Player(string ip = "localhost") : base(ip)
 		{
 			characters = new Character[NB_CHARACTERS];
+			health = MAX_HEALTH;
+
 		}
 
 		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
@@ -90,6 +93,82 @@ namespace Game
 		{
 
 		}
+
+		public void PlayGame()
+        {
+            while(!IsDead())
+            {
+				Console.WriteLine($"You have {health}HP left");
+				UpdateTeam();
+				Send(SerializedCharacters);
+
+				health--;
+            }
+        }
+
+		private void UpdateTeam()
+        {
+			ObtainNewPet();
+			LevelUpPet();
+        }
+
+		private void ObtainNewPet()
+        {
+            Ant ant = new Ant();
+			Console.WriteLine($"You received a new pet:{ant.Name}. Do you want to keep it ? [Y/N]");
+			DisplayPets();
+			if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+				Console.WriteLine($"Where do you want to place it ? [0-{NB_CHARACTERS-1}/N]");
+				ConsoleKey key = Console.ReadKey().Key;
+				while(key < ConsoleKey.D0 || key > (ConsoleKey)(NB_CHARACTERS + (int)ConsoleKey.D0))
+                {
+					Console.WriteLine("Incorrect index");
+					key = Console.ReadKey().Key;
+                }
+
+                if (characters[(key - ConsoleKey.D0)] is not null && characters[(key - ConsoleKey.D0)].Name == ant.Name)
+                {
+					characters[(key - ConsoleKey.D0)].LevelUp();
+					Console.WriteLine($"You upgraded your {ant.Name}");	
+
+				}
+				else
+                {
+					characters[(key - ConsoleKey.D0)] = ant;
+					Console.WriteLine($"{ant.Name} was added to your team");
+					DisplayPets();
+				}
+
+				
+            }
+			
+        }
+
+		private void LevelUpPet()
+        {
+			//Check character level	
+			Console.WriteLine($"Please choose a pet to upgrade [[0-{NB_CHARACTERS - 1}]");
+			ConsoleKey key = Console.ReadKey().Key;
+			if (key >= ConsoleKey.D0 && key <= (ConsoleKey)(NB_CHARACTERS + (int)ConsoleKey.D0))
+			{
+                if (characters[key - ConsoleKey.D0] is not null)
+                {
+					characters[key - ConsoleKey.D0].LevelUp();
+				}
+			}
+		}
+
+		private void DisplayPets()
+        {
+			int i = 0;
+			foreach(Character c in characters)
+            {
+				if(c is null) { i++; continue; }
+				Console.Write($"{c.Name} lvl{c.Level} [{i++}]");
+            }
+			Console.WriteLine();
+        }
 
 		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
 		|*                         PROTECTED METHODS                         *|
