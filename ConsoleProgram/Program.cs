4559f94fecc59274e63aad2a6939188bc8fbfa7d
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-06-06 13:13:11 +0200

    Update console program

--- ConsoleProgram/Program.cs
+++ ConsoleProgram/Program.cs
@@ -3,6 +3,7 @@ using Entities.Characters.Tier1;
 using Game;
 using Game.Server;
 using System;
+using System.Threading;
 
 namespace ConsoleProgram
 {
@@ -13,43 +14,64 @@ namespace ConsoleProgram
 
 		static void Main(string[] args)
 		{
-			/*
+			//RunClient();
+			RunServer();
+
+			Console.ReadLine();
+		}
+
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+		|*                          PRIVATE METHODS                          *|
+		\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+		private static void RunClient()
+		{
 			Player p = new Player("157.26.104.10");
 			p.Connect();
 
 			p.Add(new Ant(), 0);
 			p.Send(p.SerializedCharacters);
-			*/
+		}
 
-			/*
-			string asm = "Entities, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
-			string nmspace = "Entities.Characters.Tier1.Ant";
-
-			Type type = Type.GetType($"{nmspace}, {asm}");
-
-			string str = p.SerializedCharacters;
-			*/
-
-			/* Test for Hosting */
-
+		private static void RunServer()
+		{
 			Server server = new Server(1);
-			Player player = new Player(server.IP);
 
-			player.Connect();
-			server.WaitForClients();
-
-			Console.WriteLine("Le jeu démarre");
-
-			player.Add(new Ant(), 0);
-
-			while (true)
+			Thread thClient = new Thread((object data) =>
 			{
-				Console.WriteLine("******");
-				player.Send(player.SerializedCharacters);
-				server.ApplyTurn();
-			}
+				if (data is not Server server) return;
 
-			Console.ReadLine();
+				Player player = new Player(server.IP);
+
+				player.Connect();
+
+				player.Add(new Ant(), 0);
+
+				while (true)
+				{
+					Console.WriteLine("*** PLAYER ***");
+					player.Send(player.SerializedCharacters);
+					Thread.Sleep(5000);
+				}
+			});
+
+			Thread thServer = new Thread((object data) =>
+			{
+				if (data is not Server server) return;
+
+				server.WaitForClients();
+
+				Console.WriteLine("Le jeu démarre");
+
+				while (true)
+				{
+					Console.WriteLine("*** SERVER ***");
+					server.ApplyTurn();
+				}
+			});
+
+			thServer.Start(server);
+			thClient.Start(server);			
 		}
 	}
 }
