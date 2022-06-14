1ae32f3ad84a43659cae49f194950d06eb6d2324
Vincent Jeannin <vincent.jeannin@he-arc.ch>
2022-06-14 15:10:03 +0200

    Update player

--- Game/Player.cs
+++ Game/Player.cs
@@ -81,7 +81,7 @@ namespace Game
 				string[] tabStr = JsonConvert.DeserializeObject<string[]>(value);
 
 				IEnumerable<JObject> objects = tabStr.Select(c => JObject.Parse(c));
-				string[] characterNames = objects.Select(o => o.First?.ElementAt(1)?.ToString()).GetNotNullValues() ?? Array.Empty<string>();
+				string[] characterNames = objects.Select(o => o.First?.Next?.First?.ToString()).GetNotNullValues() ?? Array.Empty<string>();
 
 				if (characterNames.Length < 1) return;
 
